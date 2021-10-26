fe0053bccbf29e7b1b36efd83b14f6406aec7d49
Chan Chanakya <chanakya213@gmail.com>
2021-10-26 15:49:23 +0530

    i think this is final version

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb+srv://chanakya:chinnasandy123@cluster0.elwih.mongodb.net/todolistDB');
+mongoose.connect('mongodb+srv://chanakya:chinnasandy123@cluster0.elwih.mongodb.net/todolist?retryWrites=true&w=majority');
 
 const itemSchema = ({
     name: String
