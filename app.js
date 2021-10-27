56d2aff3011d43993d8087b2929002cde53bf788
Chan Chanakya <chanakya213@gmail.com>
2021-10-27 09:41:53 +0530

    finally fixed all the bugs

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/tododb');
+mongoose.connect("mongodb+srv://chanakya:chinnasandy123@cluster0.elwih.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(() => {
+    console.log("connection success ... .. .");
+}).catch((err) => {
+    console.log("no connection ...!");
+});
 
 const itemSchema = ({
     name: String
